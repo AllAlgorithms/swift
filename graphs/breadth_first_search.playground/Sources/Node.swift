1ad9a7a0193bcb1ec1c34a4c1ff2267e6e39e2f6
Volodymyr <klymenkodp@gmail.com>
2018-10-14 14:13:07 -0400

    Added attributes
    new attributes: color, predecessor, distance

--- graphs/breadth_first_search.playground/Sources/Node.swift
+++ graphs/breadth_first_search.playground/Sources/Node.swift
@@ -3,6 +3,9 @@ import Foundation
 public class Node{
 	public var label: String
 	public var neighbors: Array<Edge>
+	public var color: String?
+	public var predecessor: Node?
+	public var distance: Int?
 	
 	public init(_ label: String){
 		self.label = label
